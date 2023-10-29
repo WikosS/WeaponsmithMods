80facdd5b452d3bd23ffc10522c5f4d1642a20ed
WikosS <63605107+WikosS@users.noreply.github.com>
2023-10-29 19:35:59 +0100

    initial commit

---
+++ src/main/java/org/wikos/weaponsmith/Weaponsmith.java
@@ -0,0 +1,20 @@
+package org.wikos.weaponsmith;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+
+public class Weaponsmith implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    public static final Item UNDEAD_OILS = new Item(new FabricItemSettings());
+    @Override
+    public void onInitialize() {
+        Registry.register(Registries.ITEM, new Identifier("weaponsmith", "undead_oils"), UNDEAD_OILS);
+    }
+}
---
+++ src/main/java/org/wikos/weaponsmith/client/WeaponsmithClient.java
@@ -0,0 +1,13 @@
+package org.wikos.weaponsmith.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WeaponsmithClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
